40cd7f60c0357479563677a318addad61594f2a0
ErikLarssonDev <86654747+ErikLarssonDev@users.noreply.github.com>
2022-06-03 20:34:19 +0200

    Added the TourCard component.

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,50 +4,80 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import { AccessTime } from '@mui/icons-material'
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const TourCard = () => {
+
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: 'body2',
+                    },
+                    style: {
+                        fontSize: 11,
+                    }
+                },
+                {
+                    props: {
+                        variant: 'body3',
+                    },
+                    style: {
+                        fontSize: 9,
+                    }
+                }
+            ]
+        }
+    }
+})
+
+
+const TourCard = ({ tour }) => {
     return (
         <Grid item xs={3}>
-            <Paper elevation={3}>
-                <img src="https://images.pexels.com/photos/1374585/pexels-photo-1374585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className='img' ></img>
-                <Box paddingX={1} >
-                    <Typography variant='subtitle1' component='h2' >
-                        Immerse into the falls
-                    </Typography>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <AccessTime sx={{ width: 12.5 }} />
-                        <Typography variant='body2' component='p' marginLeft={0.5} >
-                            5 hours
+            <ThemeProvider theme={theme} >
+                <Paper elevation={3}>
+                    <img src={tour.image} alt='' className='img' ></img>
+                    <Box paddingX={1} >
+                        <Typography variant='subtitle1' component='h2' >
+                            {tour.name}
                         </Typography>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <AccessTime sx={{ width: 12.5 }} />
+                            <Typography variant='body2' component='p' marginLeft={0.5} >
+                                {tour.duration}
+                            </Typography>
 
+                        </Box>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}
+                            marginTop={3}
+                        >
+                            <Rating name="read-only" value={tour.rating} precision={0.5} size='small' readOnly />
+                            <Typography variant='body2' component='p' marginLeft={0.5} >
+                                {tour.rating}
+                            </Typography>
+                            <Typography variant='body3' component='p' marginLeft={1.5} >
+                                ({tour.numberOfReviews} reviews)
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant='h6' component='h3' marginTop={0} >
+                                From C ${tour.price}
+                            </Typography>
+                        </Box>
                     </Box>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center'
-                        }}
-                        marginTop={3}
-                    >
-                        <Rating name="read-only" value={4.5} precision={0.5} size='small' readOnly />
-                        <Typography variant='body2' component='p' marginLeft={0.5} >
-                            4.5
-                        </Typography>
-                        <Typography variant='body3' component='p' marginLeft={1.5} >
-                            (655 reviews)
-                        </Typography>
-                    </Box>
-                    <Box>
-                        <Typography variant='h6' component='h3' marginTop={0} >
-                            From C $147
-                        </Typography>
-                    </Box>
-                </Box>
-            </Paper>
+                </Paper>
+            </ThemeProvider>
         </Grid>
     );
 }
